a83330552e519ef9502656a495d1c5079ffa4bc5
ppgmg123 <79125967+ppgmg123@users.noreply.github.com>
2023-12-14 19:00:29 +0700

    Update createfiles.py
    Added path parser and megabytes mode

--- createfiles.py
+++ createfiles.py
@@ -1,14 +1,23 @@
-import sys
 import argparse
-def createfiles(files_amount, byte):
-    try:
+import os
+
+def createfiles(files_amount, byte, megabytes, path):
+    large_file = 0
+    
+    if path == " ":
+        path = os.path.abspath(os.getcwd())
+    
+    if megabytes == True:
+        large_file = bytearray(byte * 1024 * 1024)
+    else:
         large_file = bytes(byte)
-        for i in range(files_amount):
-            f = open(f'{i}', "wb")
-            f.write(large_file)
-        return f"Files written! Total files: {files_amount}, Total bytes: {(byte) * files_amount}"
-    except Exception as e:
-       print(f"Problems writing files: {e}")
+        
+    for i in range(1 , files_amount+1):
+        f = open(f'{os.path.join(path, f"{i}")}', "wb")
+        f.write(large_file)
+        f.close()
+            
+    return f"Files written! Total files: {files_amount}, Total bytes: {len(large_file) * files_amount}"
 
 def parsing():
     parser = argparse.ArgumentParser(
@@ -17,9 +26,12 @@ def parsing():
                     epilog='files_amount bytes')
     parser.add_argument('files_amount', help='decides how many files to create.',type=int)
     parser.add_argument('bytes', help='decides how many bytes should the file use.',type=int)
+    parser.add_argument('-m','--megabytes',help="decides if the input should be counted in megabytes instead of bytes.",
+                    action='store_true')
+    parser.add_argument('path',nargs='?', help='decides the path of the files. The default path is the path of this file.',default="")
     return parser.parse_args()
 
 if __name__ == '__main__':
     args = parsing()
-    result = createfiles(args.files_amount, args.bytes)
+    result = createfiles(args.files_amount, args.bytes, args.megabytes, args.path)
     print(result)
