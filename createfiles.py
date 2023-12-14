4e70c104baff7c6f84c68c9273c924c0c0713411
ppgmg123 <79125967+ppgmg123@users.noreply.github.com>
2023-12-14 18:09:50 +0700

    Update createfiles.py
    Specific bytes mode with several other changes

--- createfiles.py
+++ createfiles.py
@@ -1,30 +1,25 @@
 import sys
 import argparse
-def createfiles(files_amount, bytes):
+def createfiles(files_amount, byte):
     try:
-        large_file = bytearray(bytes * 1024 * 1024)
+        large_file = bytes(byte)
         for i in range(files_amount):
-           f = open(f'{i}', "wb")
-           f.write(large_file)
-        return "Files written!"
+            f = open(f'{i}', "wb")
+            f.write(large_file)
+        return f"Files written! Total files: {files_amount}, Total bytes: {(byte) * files_amount}"
     except Exception as e:
-        return f"Problems writing files: {str(e)}"
+       print(f"Problems writing files: {e}")
 
 def parsing():
     parser = argparse.ArgumentParser(
-                    prog='Create Files',
-                    description='Create files',
-                    epilog='createfiles.py files_amount bytes')
-    parser.add_argument('files_amount', help='decides how many files to create.', type=amount)
-    parser.add_argument('bytes', help='decides how many bytes should the file use.', type=bytes)
+                    prog='File creator',
+                    description='Creates a specific amount of files with specific amount of bytes.',
+                    epilog='files_amount bytes')
+    parser.add_argument('files_amount', help='decides how many files to create.',type=int)
+    parser.add_argument('bytes', help='decides how many bytes should the file use.',type=int)
     return parser.parse_args()
-    
-def totallyrandom():
-    for arg in sys.argv:
-        print(arg) 
-        
+
 if __name__ == '__main__':
-    totallyrandom()
     args = parsing()
     result = createfiles(args.files_amount, args.bytes)
     print(result)
